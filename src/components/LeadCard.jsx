88a7cf4ecc41218805e4e8fca691c3017b0d6bbc
Rohit kumar <rohitkumar9643017@gmail.com>
2025-02-07 00:54:59 +0530

    tast is completed

--- src/components/LeadCard.jsx
+++ src/components/LeadCard.jsx
@@ -1,7 +1,9 @@
 
-import { UserIcon, MailIcon, PhoneIcon, ClipboardListIcon, Trash2 } from 'lucide-react';
-
-const LeadCard = ({ lead, onDelete }) => (
+import { UserIcon, MailIcon, PhoneIcon, ClipboardListIcon, Trash2, RefreshCw  } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
+const LeadCard = ({ lead, onDelete, onUpdate }) =>{
+   const navigate = useNavigate();
+  return (
   <div className="bg-gradient-to-r from-cyan-50 to-blue-100 p-2 sm:p-6 rounded-2xl shadow-lg hover:shadow-xl w-[95%] sm:w-[35rem]">
     {/* Header Section */}
     <div className="flex justify-between items-center mb-6">
@@ -36,12 +38,20 @@ const LeadCard = ({ lead, onDelete }) => (
 
       <div className="flex items-center gap-3">
         <ClipboardListIcon className="text-blue-600" size={20} />
-        <p className="text-gray-700 font-medium">User ID: {lead.userId || "N/A"}</p>
+        <p className="text-gray-700 font-medium">Lead ID: {lead._id || "N/A"}</p>
+        {/* <p className="text-gray-700 font-medium">User ID: {lead.userId || "N/A"}</p> */}
       </div>
     </div>
 
     {/* Action Buttons */}
-    <div className="flex justify-end mt-6">
+    <div className="flex justify-end gap-4 mt-6">
+      <button
+        onClick={() =>navigate("/lead_management" , { state: { leadData: lead }})}
+        className="bg-gradient-to-r from-blue-500 to-blue-700 text-white px-3 sm:px-4 py-[0.4rem] sm:py-2 rounded-lg hover:from-blue-600 hover:to-blue-800 shadow-lg flex items-center gap-2 transition-all"
+      >
+        <RefreshCw size={18} />
+        Update
+      </button>
       <button
         onClick={() => onDelete(lead._id)}
         className="bg-gradient-to-r from-red-500 to-red-700 text-white px-3 sm:px-4 py-[0.4rem] sm:py-2 rounded-lg hover:from-red-600 hover:to-red-800 shadow-lg flex items-center gap-2 transition-all"
@@ -51,6 +61,6 @@ const LeadCard = ({ lead, onDelete }) => (
       </button>
     </div>
   </div>
-);
+)}
 
 export default LeadCard;
--- src/components/LeadForm.jsx
+++ src/components/LeadForm.jsx
@@ -1,24 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { createLeadForm } from '../libs/YupFormikValidator';
 import { Formik, Form } from 'formik'
-import { useNavigate } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom"
 import { toast } from 'react-toastify';
 import { TextInput } from './formComponents/inputs/TextInput';
 import { Button } from './formComponents/Buttons/Button';
-import { postData } from '../utils/apiCall';
+import { patchData, postData } from '../utils/apiCall';
 import { useAuthStore } from '../libs/zustand';
 
-  const LeadForm = () => {
-    const { token } = useAuthStore((state) => state);
-    const navigate = useNavigate();
+const LeadForm = () => {
+  const { token } = useAuthStore((state) => state);
+  const navigate = useNavigate();
+  const { state } = useLocation();
+  const leadData = state?.leadData;
+
+  console.log("leadData is", leadData);
   
-    // Redirect to login if the user is not logged in
-    useEffect(() => {
-      if (!token) {
-        toast.warn("Please login to access this page");
-        navigate("/login");
-      }
-    }, [token, navigate]);
+  // Redirect to login if the user is not logged in
+  useEffect(() => {
+    if (!token) {
+      toast.warn("Please login to access this page");
+      navigate("/login");
+    }
+  }, [token, navigate]);
 
   async function submitForm(values, option) {
     console.log("lead form data", values);
@@ -29,60 +33,70 @@ import { useAuthStore } from '../libs/zustand';
 
     try {
       console.log('before responce', values);
-      const myPromise = postData("/api/users/lead/create_leads", values)
+      const apiCall = leadData
+        ? patchData(`/api/users/lead/update_leads/${leadData._id}`, values)
+        : postData("/api/users/lead/create_leads", values)
 
-      console.log('after responce', myPromise);
+      console.log('after responce', apiCall);
 
       toast.promise(
-        myPromise,
+        apiCall,
         {
-          pending: 'Creating...',
-          success: 'Lead is created 👌',
+          pending: leadData ? "Updating..." : "Creating...",
+          success: leadData ? "Lead updated 👌" : "Lead created 👌",
           error: 'something went wrong.. 🤯',
         }
       );
-      const data = await myPromise;
+      const data = await apiCall;
       // if (data.success) {
       //   // console.log("success leadsss data", data);
       // }
       option.resetForm()
+      navigate("/lead_list");
+
     } catch (error) {
       toast.warn("An error occurred:" + error?.response?.data?.message)
     }
   }
- 
+
   return (
     <>
-    <div className='bg-gradient-to-r from-cyan-50 to-blue-100 p-4 rounded-xl'>
-
-   
-      <Formik
-        initialValues={createLeadForm.initialVaues}
-        validationSchema={createLeadForm.validationSchema}
-        onSubmit={submitForm}
-
-      >
-        {() => (
-          <Form>
-            <h2 className='font-inter text-[1.2rem] text-center mb-4 sm:text-[1.4rem] font-bold text-[#001A6E] my-1 tracking-wider uppercase'>
-              create Lead 
-            </h2>
+      <div className='bg-gradient-to-r from-cyan-50 to-blue-100 p-4 rounded-xl'>
 
 
-            <TextInput label="email *" name="email" type="input" />
-            <TextInput label="Name *" name="name" type="input" />
-            <TextInput label="phoneNumber *" name="phoneNumber" type="input" />
-            <TextInput
-              label="Status *"
-              name="status"
-              type="select"
-              options={["New", "Contacted", "Closed"]}
-            />
-            <Button type="submit" name={"Create lead"} style="w-full mb-3 m-0 focus:ring-orange-500" />
+        <Formik
+          initialValues={{
+            email: leadData?.email || "",
+            name: leadData?.name || "",
+            phoneNumber: leadData?.phoneNumber?.replace("+91", "") || "",
+            status: leadData?.status || "New",
+          }}
+          enableReinitialize
+          validationSchema={createLeadForm.validationSchema}
+          onSubmit={submitForm}
 
-          </Form>
-        )}
-      </Formik>
+        >
+          {() => (
+            <Form>
+              <h2 className="font-inter text-[1.2rem] text-center mb-4 sm:text-[1.4rem] font-bold text-[#001A6E] my-1 tracking-wider uppercase">
+                {leadData ? "Update Lead" : "Create Lead"}
+              </h2>
+
+
+              <TextInput label="email *" name="email" type="input" />
+              <TextInput label="Name *" name="name" type="input" />
+              <TextInput label="phoneNumber *" name="phoneNumber" type="input" />
+              <TextInput
+                label="Status *"
+                name="status"
+                type="select"
+                options={["New", "Contacted", "Closed"]}
+              />
+              <Button type="submit"  name={leadData ? "Update Lead" : "Create Lead"} style="w-full mb-3 m-0 focus:ring-orange-500" />
+
+            </Form>
+          )}
+        </Formik>
 
       </div>
     </>
